268375be5fd93c49b6b988746209cbcd4de6fd5e
Amir Bahadori <pbahadori156@gmail.com>
2016-06-04 14:41:34 -0500

    Initial Commit

---
+++ Farkle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by Pasha Bahadori on 6/4/16.
+//  Copyright © 2016 Pelican Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Farkle/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Farkle
+//
+//  Created by Pasha Bahadori on 6/4/16.
+//  Copyright © 2016 Pelican Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
